28522836f450a2fee660506829742240129c5349
Matteo DeSimone <thematteod@gmail.com>
2025-07-17 23:35:34 -0400

    Updates app startup to use config's debug setting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from server.app import app
+from server.config import get_config
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    config = get_config()
+    app.run(host="0.0.0.0", port=5000, debug=config.server.debug)
 
